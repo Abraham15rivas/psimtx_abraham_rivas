b06351f9b906db40b95e476e6a923f5603bb512a
Abraham15rivas <abraham15rivas@gmail.com>
2021-02-14 22:01:04 -0400

    create logic app and details

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header vino">{{ __('Register') }}</div>
 
                 <div class="card-body body-card">
                     <form method="POST" action="{{ route('register') }}">
